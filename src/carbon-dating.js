005501b98f94cb08ddf5a41aa1d4295301e90a13
Peter Zaichyk <zaichikpetr@gmail.com>
2022-10-11 12:43:42 +0300

    feat: count-cats and carbon-dating is ready

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if (typeof sampleActivity  !== 'string') {
+    return false
+  }
+  else {
+    let sample = +sampleActivity;
+    let t;
+    if (sample > 0 && sample <= MODERN_ACTIVITY) {
+      t = (Math.log((MODERN_ACTIVITY / sample)) * HALF_LIFE_PERIOD) / (Math.log(2));
+      return Math.ceil(t);
+    }
+      else {
+        return false;
+      }
+  }
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let counter = 0;
+ backyard.forEach(row => {
+  row.forEach(elm => {
+    if (elm === '^^') {
+      counter = counter + 1;
+    }
+    
+  })
+ });
+ return counter;
 }
 
 module.exports = {
